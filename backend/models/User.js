787bbd96ea9ce1e585bc2a0f0740fe0738bfa28f
Nikhil Golait <nikhilgolait7@gmail.com>
2025-11-14 19:33:57 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,20 +4,24 @@ const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
+    trim: true,
   },
   phone: {
     type: String,
     required: true,
-    unique: true, // ✅ make phone unique instead
-  },
-  email: {
-    type: String,
-    default: null, // ✅ no longer required
+    unique: true,
+    trim: true,
   },
   password: {
     type: String,
     required: true,
   },
+  lastMessage: {
+    type: String,
+    default: "",
+  },
+}, {
+  timestamps: true,
 });
 
 export default mongoose.model("User", userSchema);
